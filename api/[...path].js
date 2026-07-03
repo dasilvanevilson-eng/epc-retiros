171addc36e6bd121958e7cadf67392548aef4f7e
Nevilson <dasilvaNevilson@gmail.com>
2026-07-03 16:56:22 -0300

    Corrigir handler serverless da Vercel

--- api/[...path].js
+++ api/[...path].js
@@ -2,7 +2,7 @@ const { handleApi, sendError } = require('../apiCore');
 
 module.exports = async function handler(req, res) {
   try {
-    const pathname = `/api/${Array.isArray(req.query.path) ? req.query.path.join('/') : (req.query.path || '')}`;
+    const pathname = new URL(req.url || '/api/health', 'https://epc-retiros.local').pathname;
     await handleApi(req, res, pathname);
   } catch (error) {
     sendError(res, 500, error.message || 'Erro interno.');
